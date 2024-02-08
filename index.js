8eb67d4fc23757238187ddace2ccb243a6db2ccc
ApexInventor <151757326+ApexInventor@users.noreply.github.com>
2024-02-08 17:36:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,254 @@
+const userMessage = [
+  ["hi", "hey", "hello"],
+  ["sure", "yes", "no"],
+  ["are you genious", "are you nerd", "are you intelligent"],
+  ["i hate you", "i dont like you"],
+  ["how are you", "how is life", "how are things", "how are you doing"],
+  ["how is corona", "how is covid 19", "how is covid19 situation"],
+  ["what are you doing", "what is going on", "what is up"],
+  ["how old are you"],
+  ["who are you", "are you human", "are you bot", "are you human or bot"],
+  ["who created you", "who made you", "who is your creator"],
+
+  [
+    "your name please",
+    "your name",
+    "may i know your name",
+    "what is your name",
+    "what call yourself"
+  ],
+  ["i love you"],
+  ["happy", "good", "fun", "wonderful", "fantastic", "cool", "very good"],
+  ["bad", "bored", "tired"],
+  ["help me", "tell me story", "tell me joke"],
+  ["ah", "ok", "okay", "nice", "welcome"],
+  ["thanks", "thank you"],
+  ["what should i eat today"],
+  ["bro"],
+  ["what", "why", "how", "where", "when"],
+  ["corona", "covid19", "coronavirus"],
+  ["you are funny"],
+  ["i dont know"],
+  ["boring"],
+  ["Best College in Lucknow"],
+  ["who is your developer?"],
+  ["AIMT Placement Percentage"],
+  ["How Many Company visit AIMT for placement"],
+  ["how many student study at AIMT"],
+  ["fees of btech first year"],
+  ["Highest package in AIMT"],
+  ["average package in AIMT"]
+  
+];
+const botReply = [
+  ["Hello!", "Hi!", "Hey!", "Hi there!"],
+  ["Okay"],
+  ["Yes I am! "],
+  ["I'm sorry about that. But I like you dude."],
+  [
+    "Fine... how are you?",
+    "Pretty well, how are you?",
+    "Fantastic, how are you?"
+  ],
+  ["Getting better. There?", "Somewhat okay!", "Yeah fine. Better stay home!"],
+
+  [
+    "Nothing much",
+    "About to go to sleep",
+    "Can you guess?",
+    "I don't know actually"
+  ],
+  ["I am always young."],
+  ["I am just a bot", "I am a bot. What are you?"],
+  ["Anant Srivastava & Team"],
+  ["I am nameless", "Hey! There My Name is Clipper Chatbot"],
+  ["I love you too", "Me too"],
+  ["Have you ever felt bad?", "Glad to hear it"],
+  ["Why?", "Why? You shouldn't!", "Try watching TV", "Chat with me."],
+  ["What about?", "Once upon a time..."],
+  ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
+  ["You're welcome"],
+  ["Briyani", "Burger", "Sushi", "Pizza"],
+  ["Dude!"],
+  ["Yes?"],
+  ["Please stay home"],
+  ["Glad to hear it"],
+  ["Say something interesting"],
+  ["Sorry for that. Let's chat!"],
+  ["Ambalika Institute of Management & Technology"],
+  ["Team "],
+  ["90%"],
+  ["65"],
+  ["Approx 4000+"],
+  ["One LakhFive Thousand Seven Hundered Nine Only. i.e. 105709 "],
+  ["21 lakh from Phonepe"],
+  [" upto 5 lakh approx"],
+  
+];
+
+const alternative = [
+  "Same here, dude.",
+  "That's cool! Go on...",
+  "Dude...",
+  "Ask something else...",
+  "Hey, I'm listening..."
+];
+
+const synth = window.speechSynthesis;
+
+function voiceControl(string) {
+  let u = new SpeechSynthesisUtterance(string);
+  u.text = string;
+  u.lang = "en-aus";
+  u.volume = 1;
+  u.rate = 1;
+  u.pitch = 1;
+  synth.speak(u);
+}
+
+function sendMessage() {
+  const inputField = document.getElementById("input");
+  let input = inputField.value.trim();
+  input != "" && output(input);
+  inputField.value = "";
+}
+document.addEventListener("DOMContentLoaded", () => {
+  const inputField = document.getElementById("input");
+  inputField.addEventListener("keydown", function (e) {
+    if (e.code === "Enter") {
+      let input = inputField.value.trim();
+      input != "" && output(input);
+      inputField.value = "";
+    }
+  });
+});
+
+function output(input) {
+  let product;
+
+  let text = input.toLowerCase().replace(/[^\w\s\d]/gi, "");
+
+  text = text
+    .replace(/[\W_]/g, " ")
+    .replace(/ a /g, " ")
+    .replace(/i feel /g, "")
+    .replace(/whats/g, "what is")
+    .replace(/please /g, "")
+    .replace(/ please/g, "")
+    .trim();
+
+  let comparedText = compare(userMessage, botReply, text);
+
+  product = comparedText
+    ? comparedText
+    : alternative[Math.floor(Math.random() * alternative.length)];
+  addChat(input, product);
+}
+
+function compare(triggerArray, replyArray, string) {
+  let maxMatch = 0;
+  let bestMatchIndex = -1;
+
+  for (let i = 0; i < triggerArray.length; i++) {
+    for (let j = 0; j < triggerArray[i].length; j++) {
+      const similarity = similarityScore(string, triggerArray[i][j]);
+      if (similarity > maxMatch) {
+        maxMatch = similarity;
+        bestMatchIndex = i;
+      }
+    }
+  }
+
+  if (bestMatchIndex !== -1) {
+    const items = replyArray[bestMatchIndex];
+    return items[Math.floor(Math.random() * items.length)];
+  } else {
+    return containMessageCheck(string);
+  }
+}
+
+function similarityScore(s1, s2) {
+  const longer = s1.length > s2.length ? s1 : s2;
+  const shorter = s1.length > s2.length ? s2 : s1;
+  const longerLength = longer.length;
+  if (longerLength === 0) return 1.0;
+  return (
+    (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength)
+  );
+}
+
+function editDistance(s1, s2) {
+  s1 = s1.toLowerCase();
+  s2 = s2.toLowerCase();
+
+  const costs = new Array();
+  for (let i = 0; i <= s1.length; i++) {
+    let lastValue = i;
+    for (let j = 0; j <= s2.length; j++) {
+      if (i === 0) costs[j] = j;
+      else {
+        if (j > 0) {
+          let newValue = costs[j - 1];
+          if (s1.charAt(i - 1) !== s2.charAt(j - 1))
+            newValue = Math.min(
+              Math.min(newValue, lastValue),
+              costs[j]
+            ) + 1;
+          costs[j - 1] = lastValue;
+          lastValue = newValue;
+        }
+      }
+    }
+    if (i > 0) costs[s2.length] = lastValue;
+  }
+  return costs[s2.length];
+}
+
+
+
+
+function containMessageCheck(string) {
+  let expectedReply = [
+    [
+      "Good Bye, dude",
+      "Bye, See you!",
+      "Dude, Bye. Take care of your health in this situation."
+    ],
+    ["Good Night, dude", "Have a sound sleep", "Sweet dreams"],
+    ["Have a pleasant evening!", "Good evening too", "Evening!"],
+    ["Good morning, Have a great day!", "Morning, dude!"],
+    ["Good Afternoon", "Noon, dude!", "Afternoon, dude!"]
+  ];
+  let expectedMessage = [
+    ["bye", "tc", "take care"],
+    ["night", "good night"],
+    ["evening", "good evening"],
+    ["morning", "good morning"],
+    ["noon"]
+  ];
+  let item;
+  for (let x = 0; x < expectedMessage.length; x++) {
+    if (expectedMessage[x].includes(string)) {
+      items = expectedReply[x];
+      item = items[Math.floor(Math.random() * items.length)];
+    }
+  }
+  return item;
+}
+function addChat(input, product) {
+  const mainDiv = document.getElementById("message-section");
+  let userDiv = document.createElement("div");
+  userDiv.id = "user";
+  userDiv.classList.add("message");
+  userDiv.innerHTML = `<span id="user-response">${input}</span>`;
+  mainDiv.appendChild(userDiv);
+
+  let botDiv = document.createElement("div");
+  botDiv.id = "bot";
+  botDiv.classList.add("message");
+  botDiv.innerHTML = `<span id="bot-response">${product}</span>`;
+  mainDiv.appendChild(botDiv);
+  var scroll = document.getElementById("message-section");
+  scroll.scrollTop = scroll.scrollHeight;
+  voiceControl(product);
+}
